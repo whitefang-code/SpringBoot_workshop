0df3911a8aee2864de21b81fbfabac5fb5ccbf85
whitefang-code <lucianoarn5@gmail.com>
2023-01-18 00:32:28 -0300

    Project created

---
+++ src/test/java/com/arn/luc/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arn.luc.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
