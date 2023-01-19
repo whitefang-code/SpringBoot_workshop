05f6431c4aa83916b4c7af86f1eb6c7d08571402
whitefang-code <lucianoarn5@gmail.com>
2023-01-19 02:06:19 -0300

    Order , Instant, ISO 8601 - PART 1

--- src/main/java/com/arn/luc/course/entities/User.java
+++ src/main/java/com/arn/luc/course/entities/User.java
@@ -1,15 +1,18 @@
 package com.arn.luc.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 @Entity
-@Table(name="tp_user")
+@Table(name="tb_user")
 public class User implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
@@ -20,6 +23,9 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -78,6 +84,11 @@ public class User implements Serializable{
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 
 	@Override
 	public boolean equals(Object obj) {
@@ -90,6 +101,7 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
 	
 	
 
