9f1d5a1c541728e3973090a8be339fe730965022
whitefang-code <lucianoarn5@gmail.com>
2023-01-20 18:44:49 -0300

    exception Handling - findById

--- src/main/java/com/arn/luc/course/services/UserService.java
+++ src/main/java/com/arn/luc/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.arn.luc.course.entities.User;
 import com.arn.luc.course.repositories.UserRepository;
+import com.arn.luc.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	public User findById(Long id) {
 		
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 		
 	}
 	
---
+++ src/main/java/com/arn/luc/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package com.arn.luc.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found . Id" + id);
+		
+	}
+	
+
+}
