39090913066ebd11ae02068868193e9cd51a7a76
whitefang-code <lucianoarn5@gmail.com>
2023-01-20 20:29:15 -0300

    exception handling - update

--- src/main/java/com/arn/luc/course/services/UserService.java
+++ src/main/java/com/arn/luc/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.arn.luc.course.repositories.UserRepository;
 import com.arn.luc.course.services.exceptions.DatabaseException;
 import com.arn.luc.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -46,9 +48,14 @@ public class UserService {
 	}
 	
 	public User update(Long id,User obj) {
+	 try {	
 		User entity = repository.getReferenceById(id);
 		updateData(entity,obj);
 		return repository.save(entity);
+	 }catch (EntityNotFoundException e) {
+		
+		throw new ResourceNotFoundException(id);
+	 }
 	}
 
 	private void updateData(User entity, User obj) {
